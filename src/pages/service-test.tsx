a537a2a119dbd3ff9e95e6746413e22f5bb5e684
Vilius Linge <viliusling@gmail.com>
2023-11-28 16:27:13 +0200

    Implement api service

---
+++ src/pages/service-test.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import { Box, Button } from "@chakra-ui/react";
+
+export const Head: HeadFC = () => <title>Astropal</title>;
+
+import { getService } from "@utils/service";
+
+const ServiceTestPage: React.FC<PageProps> = () => {
+  const service = getService({ mock: true });
+
+  return (
+    <Box>
+      Hello
+      <Button
+        onClick={async () => {
+          const res = await service.getDailyPhrase();
+          console.log(res);
+        }}
+      >
+        Daily phrase
+      </Button>
+      <Button
+        onClick={async () => {
+          const res = await service.getZodiacSignData("gemini");
+          console.log(res);
+        }}
+      >
+        Sign
+      </Button>
+      <Button
+        onClick={async () => {
+          const res = await service.getNumerologyData();
+          console.log(res);
+        }}
+      >
+        Numerology
+      </Button>
+    </Box>
+  );
+};
+
+export default ServiceTestPage;
