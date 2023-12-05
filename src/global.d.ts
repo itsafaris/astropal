ac4b08d9d90845fe79189b01bb72db5f57390dd2
martynasj <martyjankauskas@gmail.com>
2023-12-05 17:48:48 +0200

    global gif

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,11 @@ declare module "*.png" {
   export = value;
 }
 
+declare module "*.gif" {
+  const value: string;
+  export = value;
+}
+
 declare module "*.jpg" {
   const value: string;
   export = value;
