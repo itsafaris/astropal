1153f0616f459ba53d41e8b04fb892f264f93c44
martynasj <martyjankauskas@gmail.com>
2023-12-12 16:36:17 +0200

    is prod mode

--- src/components/quizpage/quizWrapper.tsx
+++ src/components/quizpage/quizWrapper.tsx
@@ -3,12 +3,13 @@ import { QuizProvider, QuizProps } from "@martynasj/quiz-lib";
 const locationApiKey = "pk.ce6e81605ad27d8ee1815287902636e1";
 
 import { QuizServiceWrapper } from "./quizServiceWrapper";
+import { isProdMode } from "@utils/isProdMode";
 
 export function QuizWrapper(props: Omit<QuizProps, "locationApiKey">) {
   return (
     <QuizServiceWrapper>
       <QuizProvider
-        showDebugUI
+        showDebugUI={!isProdMode()}
         onErrorEvent={(_) => {
           //
         }}
