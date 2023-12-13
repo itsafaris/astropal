a0ede7c8249bfbc529f2aa7b8ee74ae1efc7a028
martynasj <martyjankauskas@gmail.com>
2023-12-13 16:56:06 +0200

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { SEO } from "@components/seo";
 import { useSiteMetadata } from "@hooks/useSiteMetadata";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import { PropsWithChildren } from "react";
 
 const HEADING_TITLE = "Personalized astrology report with powerful predictions";
 
@@ -17,29 +19,55 @@ export function Head() {
 
 export default function IndexPage() {
   return (
-    <Box py={16} px={8} maxW="xl" mx="auto">
-      <Heading mb={12} as="h1" size="xl" textAlign="center">
-        {HEADING_TITLE}
-      </Heading>
-      <Text my={8} fontSize="lg" textAlign="center">
-        Complete a{" "}
-        <Text as="span" color="bra" fontWeight={"bold"}>
-          1-minute
-        </Text>{" "}
-        quiz to get a personalized prediction. The result is not guaranteed and may vary from case
-        to case.
-      </Text>
-      <Text textAlign={"center"} fontWeight={"bold"}>
-        What is your relationship status?
-      </Text>
-      <Flex justifyContent="center" mt={8} gap={4}>
-        <Button as={Link} to="/quiz-single" colorScheme="brand">
-          Single
-        </Button>
-        <Button as={Link} to="/quiz-inrelationship" colorScheme="teal">
-          In relationship
-        </Button>
-      </Flex>
+    <Box minHeight={"100vh"} color="bg.900" bgGradient={"linear(to-b, bg.50, bg.100)"}>
+      <Box py={16} px={8} maxWidth="md" mx="auto">
+        <Heading mb={12} as="h1" size="xl" textAlign="center">
+          {HEADING_TITLE}
+        </Heading>
+        <Text my={8} fontSize="lg" textAlign="center" color="bg.400">
+          Complete a{" "}
+          <Text as="span" fontWeight={"bold"} color="brand.500">
+            1-minute
+          </Text>{" "}
+          quiz to get a personalized prediction. The result is not guaranteed and may vary from case
+          to case.
+        </Text>
+        <Text textAlign={"center"} fontSize={"xl"} fontWeight={"bold"}>
+          What is your relationship status?
+        </Text>
+        <Flex justifyContent="center" mt={8} gap={8}>
+          <Link to="/quiz-single">
+            <ImageButton text="Single">
+              <StaticImage src={`../images/single_person.png`} alt="Option - single" />
+            </ImageButton>
+          </Link>
+          <Link to="/quiz-inrelationship">
+            <ImageButton text="In relationship">
+              <StaticImage src={`../images/inrelationship.png`} alt="Option - in relationship" />
+            </ImageButton>
+          </Link>
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
+
+function ImageButton({ children, text }: PropsWithChildren<{ text: string }>) {
+  return (
+    <Box>
+      <Box
+        p={4}
+        borderRadius={"2xl"}
+        overflow={"hidden"}
+        border="1px solid"
+        borderColor={"brand.200"}
+        boxShadow={"0 0 8px 4px rgb(40 86 99 / 20%)"}
+      >
+        {children}
+        <Text mt={4} textAlign={"center"} color="brand.400" fontWeight={"semibold"}>
+          {text}
+        </Text>
+      </Box>
     </Box>
   );
 }
