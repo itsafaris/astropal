109b95fed35320933bb6a7db0abf7df4c4f14639
Vilius Linge <viliusling@gmail.com>
2023-12-30 17:02:11 +0200

    finish setuping openai service

--- src/services/openaiService.ts
+++ src/services/openaiService.ts
@@ -1,52 +1,53 @@
-// import OpenAI from "openai";
+import OpenAI from "openai";
 
-// const API_KEY = "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";
-// const ORG_ID = "org-sP7lZMPWGsGm3j2vrN1WD05D";
+const ORG_ID = "org-sP7lZMPWGsGm3j2vrN1WD05D";
 
-// interface Service {
-//   stream: (prompt: string, onContent: (content: string) => void) => AbortController["abort"];
-// }
+interface Service {
+  stream: (prompt: string, onContent: (content: string) => void) => AbortController["abort"];
+}
 
-// function createOpenai(): OpenAI {
-//   return new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY,
-//     organization: ORG_ID,
-//     dangerouslyAllowBrowser: true,
-//   });
-// }
+function createOpenai(): OpenAI {
+  return new OpenAI({
+    apiKey: process.env.GATSBY_OPENAI_API_KEY,
+    organization: ORG_ID,
+    dangerouslyAllowBrowser: true,
+  });
+}
 
-// // const service: Service = {
-// //   stream: (prompt, onContent) => {
-// //     const openai = createOpenai();
-// //     const s = openai.beta.chat.completions.stream({
-// //       model: "gpt-4",
-// //       messages: [
-// //         {
-// //           role: "user",
-// //           content: prompt,
-// //         },
-// //       ],
-// //       stream: true,
-// //     });
+const service: Service = {
+  stream: (prompt, onContent) => {
+    const openai = createOpenai();
+    const s = openai.beta.chat.completions.stream({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      stream: true,
+    });
 
-// //     s.on("content", (_delta, snapshot) => {
-// //       onContent(snapshot);
-// //     });
+    s.on("content", (_delta, snapshot) => {
+      onContent(snapshot);
+    });
 
-// //     s.on("chatCompletion", () => {
-// //       s.controller.abort();
-// //     });
+    s.on("chatCompletion", () => {
+      s.controller.abort();
+    });
 
-// //     return s.controller.abort;
-// //   },
-// // };
+    return s.controller.abort;
+  },
+};
 
-const service = {
-  stream: () => {
-    console.log(process.env.GATSBY_OPENAI_API_KEY);
+const serviceMock: Service = {
+  stream: (_, onContent) => {
+    onContent("Hello");
+
+    return () => {};
   },
 };
 
 export function getOpenaiService({ mock = false }: { mock: boolean }) {
-  return mock ? service : service;
+  return mock ? serviceMock : service;
 }
