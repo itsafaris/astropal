71f03291d60b14ef7b0c8342cf930b2eb2e5430e
martynasj <martyjankauskas@gmail.com>
2024-01-02 14:36:09 +0200

    use rest api

--- src/components/quizpage/slide.firstInterpretation.tsx
+++ src/components/quizpage/slide.firstInterpretation.tsx
@@ -5,7 +5,7 @@ import { getPersonalInfoFromState } from "@utils/state";
 
 import { Subtitle } from "./components";
 import { createNatalChartData } from "@utils/natalChart";
-import { getOpenaiService } from "@services/openaiService";
+import { getOpenaiService, isApiError } from "@services/openaiService";
 
 export function NatalChartInterpretationSlide() {
   return (
@@ -29,24 +29,34 @@ function NatalChartInterpreter() {
 
   React.useEffect(() => {
     if (state.currentSlideID === "natal-chart-interpretation") {
-      const { yourBirthDate, yourBirthTime, yourBirthLocation } = getPersonalInfoFromState(
-        state.slideStateByID
-      );
+      (async function () {
+        const { yourBirthDate, yourBirthTime, yourBirthLocation } = getPersonalInfoFromState(
+          state.slideStateByID
+        );
 
-      const natalChart = createNatalChartData({
-        year: yourBirthDate.year,
-        month: yourBirthDate.month,
-        date: yourBirthDate.day,
-        hour: yourBirthTime.time24.hour,
-        minute: yourBirthTime.time24.minute,
-        latitude: yourBirthLocation.lat,
-        longitude: yourBirthLocation.long,
-      });
+        const natalChart = createNatalChartData({
+          year: yourBirthDate.year,
+          month: yourBirthDate.month,
+          date: yourBirthDate.day,
+          hour: yourBirthTime.time24.hour,
+          minute: yourBirthTime.time24.minute,
+          latitude: yourBirthLocation.lat,
+          longitude: yourBirthLocation.long,
+        });
 
-      const openai = getOpenaiService({ mock: false });
-      openai.stream(natalChart, "Describe my personality", (text) => {
-        setInterpretation(text);
-      });
+        const openai = getOpenaiService({ mock: false });
+        const PROMPT = "Describe my personality";
+
+        try {
+          const answer = await openai.fetchAnswer(natalChart, PROMPT);
+          setInterpretation(answer);
+        } catch (err) {
+          if (isApiError(err)) {
+            setInterpretation(err.message);
+          }
+          throw err;
+        }
+      })();
     }
   }, [state.currentSlideID]);
 
--- src/services/openaiService.ts
+++ src/services/openaiService.ts
@@ -1,7 +1,17 @@
 import { NatalChart } from "@utils/natalChart";
 import OpenAI from "openai";
 
-const ORG_ID = "org-sP7lZMPWGsGm3j2vrN1WD05D";
+const ORG_ID = "org-aU2Uwbda3KxT1RXXw1i8DILB";
+const GPT_MODEL = "gpt-4-1106-preview";
+
+type ApiError = {
+  message: string;
+  type: string;
+};
+
+export function isApiError(err: unknown): err is ApiError {
+  return typeof err == "object" && err != null && "message" in err;
+}
 
 function createPrompt(natalChart: NatalChart, question: string): string {
   return `
@@ -13,13 +23,7 @@ function createPrompt(natalChart: NatalChart, question: string): string {
   `;
 }
 
-interface Service {
-  stream: (
-    natalChart: NatalChart,
-    question: string,
-    onAnswer: (text: string) => void
-  ) => AbortController["abort"];
-}
+type Service = typeof service;
 
 function createOpenai(): OpenAI {
   return new OpenAI({
@@ -29,11 +33,23 @@ function createOpenai(): OpenAI {
   });
 }
 
-const service: Service = {
-  stream: (natalChart, question, onAnswer) => {
+const service = {
+  async fetchAnswer(natalChart: NatalChart, question: string) {
+    const prompt = createPrompt(natalChart, question);
+    return fetchCompletion({
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+  },
+
+  stream: (natalChart: NatalChart, question: string, onAnswer) => {
     const openai = createOpenai();
     const s = openai.beta.chat.completions.stream({
-      model: "gpt-4-1106-preview",
+      model: GPT_MODEL,
       messages: [
         {
           role: "user",
@@ -55,7 +71,41 @@ const service: Service = {
   },
 };
 
+async function fetchCompletion(input: {
+  messages: Array<{ role: "user" | "system"; content: string }>;
+}): Promise<string> {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.GATSBY_OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: GPT_MODEL,
+        messages: input.messages,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+      const err = data.error as ApiError;
+      throw err;
+    }
+
+    return data.choices[0].message.content;
+  } catch (error) {
+    console.error("There was an error fetching the completion:", error);
+    throw error;
+  }
+}
+
 const serviceMock: Service = {
+  fetchAnswer: async () => {
+    return "hello, this is a mocked advice";
+  },
+
   stream: (_natalChart, _question, onAnswer) => {
     onAnswer("Hello");
 
