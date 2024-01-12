485c3dddd49eec4117a073711068ef4dbd27b648
Vilius Linge <viliusling@gmail.com>
2024-01-12 18:05:46 +0200

    update media coverage section styles

--- src/components/summary/MediaCoverageSection.tsx
+++ src/components/summary/MediaCoverageSection.tsx
@@ -3,25 +3,27 @@ import { StaticImage } from "gatsby-plugin-image";
 
 export function MediaCoverageSection() {
   return (
-    <Stack id="media-coverage" gap={6} alignItems={"center"} my={10}>
-      <Text fontSize={"md"} color="bg.500" fontWeight={"semibold"}>
+    <Stack id="media-coverage" gap={7} alignItems={"center"} my={10}>
+      <Text fontSize={"md"} color="bg.900" fontWeight={"semibold"}>
         Technology used is featured in:
       </Text>
 
-      <Flex width={"200px"} opacity={0.8}>
-        <StaticImage alt="New york times" src="../../images/media-nyt.svg" />
-      </Flex>
-
-      <Flex width={"120px"} opacity={0.8}>
-        <StaticImage alt="Cosmopolitan" src="../../images/media-cosmopolitan.svg" />
-      </Flex>
-
-      <Flex width={"90px"} opacity={0.8}>
-        <StaticImage alt="Vogue" src="../../images/media-vogue.svg" />
-      </Flex>
-
-      <Flex width={"100px"} opacity={0.8}>
+      <Flex width={"100px"} opacity={1}>
         <StaticImage alt="The guardian" src="../../images/media-guardian.svg" />
+      </Flex>
+
+      <Flex flexDirection={"row"} alignItems={"center"} gap={6}>
+        <Flex width={"120px"} opacity={1}>
+          <StaticImage alt="Cosmopolitan" src="../../images/media-cosmopolitan.svg" />
+        </Flex>
+
+        <Flex width={"90px"} opacity={1}>
+          <StaticImage alt="Vogue" src="../../images/media-vogue.svg" />
+        </Flex>
+      </Flex>
+
+      <Flex width={"200px"} opacity={1}>
+        <StaticImage alt="New york times" src="../../images/media-nyt.svg" />
       </Flex>
     </Stack>
   );
