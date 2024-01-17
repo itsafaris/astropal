8ac644a620795bc41ec9b0e0b0e1712656b98095
Vilius Linge <viliusling@gmail.com>
2024-01-17 11:25:10 +0200

    Disable copying of domain ownership

--- copyDomainOwnerhship.ts
+++ copyDomainOwnerhship.ts
@@ -1,11 +1,11 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-const f = "./apple-developer-merchantid-domain-association";
-const dist = "./public/.well-known";
+// const f = "./apple-developer-merchantid-domain-association";
+// const dist = "./public/.well-known";
 
-if (!fs.existsSync(dist)) {
-  fs.mkdirSync(dist, { recursive: true });
-}
+// if (!fs.existsSync(dist)) {
+//   fs.mkdirSync(dist, { recursive: true });
+// }
 
-fs.copyFileSync(f, path.join(dist, f));
+// fs.copyFileSync(f, path.join(dist, f));
