285c20038dd22d9fe8b6fca0b74956fdb7132c24
Vilius Linge <viliusling@gmail.com>
2024-01-22 16:12:36 +0200

    update orb styles

--- src/components/Orb.tsx
+++ src/components/Orb.tsx
@@ -2,13 +2,13 @@ import { Box, BoxProps, keyframes } from "@chakra-ui/react";
 
 const animationKeyframes = keyframes`
 0% {
-  box-shadow: 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff, 0px 0px 10px 0px #a2e0ffb3;
+  box-shadow: inset 0px 0px 10px 5px #00abff, 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff, 0px 0px 10px 0px #a2e0ffb3;
 }
 50% {
-  box-shadow: 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff, 0px 0px 50px 0px #a2e0ffb3;
+  box-shadow: inset 0px 0px 10px 5px #00abff, 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff, 0px 0px 50px 0px #a2e0ffb3;
 }
 100% {
-  box-shadow: 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff, 0px 0px 10px 0px #a2e0ffb3;
+  box-shadow: inset 0px 0px 10px 5px #00abff, 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff, 0px 0px 10px 0px #a2e0ffb3;
 }
 `;
 
@@ -26,8 +26,11 @@ export function Orb({
       height={sizeString}
       width={sizeString}
       borderRadius={"50%"}
-      backgroundColor={"#cdfdff"}
-      boxShadow={"0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff"}
+      // backgroundColor={"#cdfdff"}
+      border={"3px solid #cdfdff"}
+      boxShadow={
+        "inset 0px 0px 10px 5px #00abff, 0px 0px 50px 0px #00abff, 0px 0px 10px 0px #0ab2ff"
+      }
       animation={enableAnimation ? animation : undefined}
       {...rest}
     />
