ba22d639ab18bbd6877da656c24a658c1c728100
Vilius Linge <viliusling@gmail.com>
2024-01-26 17:16:49 +0200

    update texts

--- src/components/quizpage/slides/IntroSlide.tsx
+++ src/components/quizpage/slides/IntroSlide.tsx
@@ -12,7 +12,7 @@ export function IntroSlide() {
   return (
     <Slide id="intro" type="filler">
       <ChatBubble
-        text={`Hey, there! 👋 We have helped more than 31000 souls to explore and understand themselves better so we can help you too. By the end of the quiz you will have:`}
+        text={`Hey there! We have helped more than 31,000 souls explore and understand themselves better, so we can help you too 👋`}
         instant={showInput}
         onFinishedTyping={() => {
           setTimeout(() => {
@@ -22,29 +22,8 @@ export function IntroSlide() {
       />
       {showInput && (
         <>
-          <Stack textAlign={"center"} color="white" fontSize={"lg"}>
-            <Text>
-              1. Created full{" "}
-              <Text as="span" color="brand.600" fontWeight={"bold"}>
-                Astrological Profile
-              </Text>
-            </Text>
-            <Text>
-              2. Identified{" "}
-              <Text as="span" color="brand.600" fontWeight={"bold"}>
-                Life Challenges
-              </Text>
-            </Text>
-            <Text>
-              3. Crafted{" "}
-              <Text as="span" color="brand.600" fontWeight={"bold"}>
-                Self-Discovery Guide
-              </Text>
-            </Text>
-          </Stack>
-
           <Flex
-            my={5}
+            mb={5}
             height={100}
             width={100}
             borderRadius={"50%"}
@@ -53,8 +32,30 @@ export function IntroSlide() {
             alignItems={"center"}
             mx={"auto"}
           >
-            <StaticImage alt="" src="../../../images/calm-woman-2.png" />
+            <StaticImage alt="" src="../../../images/astro_profile.png" />
           </Flex>
+
+          <Stack textAlign={"center"} color="white" fontSize={"lg"} mb={5} spacing={1}>
+            <Text mb={2}>We are here to:</Text>
+            <Text>
+              1. Create your{" "}
+              <Text as="span" color="brand.600" fontWeight={"bold"}>
+                Astrological Profile
+              </Text>
+            </Text>
+            <Text>
+              2. Identify{" "}
+              <Text as="span" color="brand.600" fontWeight={"bold"}>
+                Life Challenges
+              </Text>
+            </Text>
+            <Text>
+              3. Craft a{" "}
+              <Text as="span" color="brand.600" fontWeight={"bold"}>
+                Self-Discovery Guide
+              </Text>
+            </Text>
+          </Stack>
 
           <NextButton
             onClick={() => {
