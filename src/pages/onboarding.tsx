688be8f147641b0530a315a6fd0820c596513c37
Vilius Linge <viliusling@gmail.com>
2024-01-26 11:37:04 +0200

    Remove answer longevity slide

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -80,7 +80,7 @@ export default function OnboardingQuiz() {
             <DescribeYourNatalChart />
             <FirstQuestionTrial />
             <SatisfactionScoreSlide />
-            <AsnwerLongevity />
+            {/* <AsnwerLongevity /> */}
             <SocialProofFiller />
             <IntroToFinetuningPart />
             <PersonalGrowthScore />
