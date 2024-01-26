a93053009b7749dfde5f2530a3eea1605aee0bd3
Vilius Linge <viliusling@gmail.com>
2024-01-26 10:39:00 +0200

    Enable autoproceed on profile saving slide

--- src/components/quizpage/slides/YourProfileSavingSlide.tsx
+++ src/components/quizpage/slides/YourProfileSavingSlide.tsx
@@ -7,7 +7,7 @@ export function YourProfileSavingSlide() {
   const [showInput, setShowInput] = useState(false);
 
   return (
-    <Slide id="your-profile-saving" type="loading" duration={5}>
+    <Slide id="your-profile-saving" type="loading" duration={5} autoProceed>
       <ChatBubble
         text="Take a brief pause while we collect the celestial insights 😌"
         instant={showInput}
