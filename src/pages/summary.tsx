08b83bd16c5d4ec3bbe55d2e8c9ae3c9c5b459bb
martynasj <martyjankauskas@gmail.com>
2024-01-31 09:46:54 +0200

    summary as in v2

--- src/pages/summary.tsx
+++ src/pages/summary.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
-import { Box, Container, Text } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { PageProps } from "gatsby";
 
 import { TopNavigation } from "@components/topnavigation";
 import { loadQuizState } from "@utils/localStorage";
 import { QuizStateParsed } from "@utils/state";
 
+import { AreasOfGuidanceSection } from "@components/summary/AreasOfGuidanceSection";
 import { AstrologerComparisonSection } from "@components/summary/AstrologerComparisonSection";
+import { ExampleQuestionsSection } from "@components/summary/ExampleQuestionsSection";
 import { MediaCoverageSection } from "@components/summary/MediaCoverageSection";
+import { SimilarUsersLikeYouSection } from "@components/summary/SimilarUsersLikeYouSection";
 import { TestimonialsSection } from "@components/summary/TestimonialsSection";
+import { HeroSection } from "@components/summary/HeroSection";
 import { PricingSection } from "@components/summary/PricingSection";
 import { SpecialOfferBanner } from "@components/summary/SpecialOfferBanner";
 
@@ -21,68 +25,23 @@ export default function SummaryPage({}: PageProps) {
   }, []);
 
   return (
-    <Box pb={24} backgroundColor="bg.50" color="bg.900" scrollBehavior={"smooth"}>
-      <Box backgroundColor="bg.50">
-        <Container>
-          <TopNavigation />
-        </Container>
-      </Box>
-
-      {/* <HeroSection2 quizState={quizState} /> */}
-
-      <Container
-        border={"1px solid white"}
-        height={500}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Text textAlign={"center"} fontSize={"3xl"} color="white">
-          Highly personalized title with a BIG promise
-        </Text>
+    <Box
+      py={4}
+      pb={24}
+      bgGradient="linear(to-b, bg.50, bg.100)"
+      color="bg.900"
+      scrollBehavior={"smooth"}
+    >
+      <Container>
+        <TopNavigation />
       </Container>
 
-      <Container
-        border={"1px solid white"}
-        height={500}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Text textAlign={"center"} fontSize={"3xl"} color="white">
-          Chart showing self-discovery progress when you follow the guide
-        </Text>
-      </Container>
-
-      <Container
-        border={"1px solid white"}
-        height={500}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Text textAlign={"center"} fontSize={"3xl"} color="white">
-          No more issues if you follow the guide
-        </Text>
-      </Container>
-
-      <Container
-        border={"1px solid white"}
-        height={500}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Text textAlign={"center"} fontSize={"3xl"} color="white">
-          App features explained. Next to each feature show a perfect example of an insight, chat
-          and etc
-        </Text>
-      </Container>
+      <HeroSection quizState={quizState} />
 
       <Container>
-        {/* <AreasOfGuidanceSection /> */}
-        {/* <ExampleQuestionsSection /> */}
-        {/* <SimilarUsersLikeYouSection /> */}
+        <AreasOfGuidanceSection />
+        <ExampleQuestionsSection />
+        <SimilarUsersLikeYouSection />
         <TestimonialsSection />
         <MediaCoverageSection />
       </Container>
@@ -92,18 +51,6 @@ export default function SummaryPage({}: PageProps) {
       <Container>
         <PricingSection />
         <AstrologerComparisonSection />
-      </Container>
-
-      <Container
-        border={"1px solid white"}
-        height={500}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Text textAlign={"center"} fontSize={"3xl"} color="white">
-          Final CTA
-        </Text>
       </Container>
     </Box>
   );
