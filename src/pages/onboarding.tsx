3901a0a779f65d1aa5b0ae13d35ed2e790b7d0d8
Vilius Linge <viliusling@gmail.com>
2024-02-02 11:25:53 +0200

    Update

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -73,7 +73,7 @@ export default function OnboardingQuiz() {
           }}
         >
           <Segment title="Progress">
-            <IntroSlide />
+            {/* <IntroSlide /> */}
             <YourBirthDateSlide />
             <YourBirthTimeSlide />
             <YourBirthPlaceSlide />
