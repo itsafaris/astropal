e1d39baf6202e5a27dbf9a50c870252c7d384c34
Vilius Linge <viliusling@gmail.com>
2024-02-06 16:45:48 +0200

    Create testimonial slide show

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -11,34 +11,25 @@ export type Testimonial = {
 
 export const TestimonialCard = ({ testimonial }: { testimonial: Testimonial }) => {
   return (
-    <Box color="black" boxShadow={"lg"} p={4} bg="white">
-      <Stack spacing={2} direction={"row"}>
-        <Box
-          flexShrink={0}
-          width={8}
-          height={8}
-          bg="blue"
-          borderRadius={"full"}
-          overflow={"hidden"}
-        >
-          {testimonial.avatar}
-        </Box>
-        <Stack>
-          <Stack spacing={0} bg={"gray.100"} px={3} py={2} borderRadius={18}>
-            <Text color="black" fontWeight={600} fontSize={"xs"}>
-              {testimonial.name}
-            </Text>
-            <Text fontSize={"xs"}>{testimonial.quote}</Text>
-          </Stack>
-          <Stack px={2} direction={"row"} fontSize={"xs"} color="gray.500" fontWeight={"bold"}>
-            <Text color="gray.700" fontWeight={"thin"}>
-              {testimonial.commentAge}
-            </Text>
-            <Text>Like</Text>
-            <Text>Reply</Text>
-          </Stack>
+    <Stack spacing={2} direction={"row"}>
+      <Box flexShrink={0} width={8} height={8} bg="blue" borderRadius={"full"} overflow={"hidden"}>
+        {testimonial.avatar}
+      </Box>
+      <Stack>
+        <Stack spacing={0} bg={"gray.200"} px={4} py={2} borderRadius={"14px"}>
+          <Text color="black" fontWeight={600} fontSize={"13px"}>
+            {testimonial.name}
+          </Text>
+          <Text fontSize={"13px"}>{testimonial.quote}</Text>
+        </Stack>
+        <Stack px={2} direction={"row"} fontSize={"xs"} color="gray.500" fontWeight={"bold"}>
+          <Text color="gray.700" fontWeight={"regular"}>
+            {testimonial.commentAge}
+          </Text>
+          <Text>Like</Text>
+          <Text>Reply</Text>
         </Stack>
       </Stack>
-    </Box>
+    </Stack>
   );
 };
--- src/components/quizpage/slides/SavingProfileSlide.tsx
+++ src/components/quizpage/slides/SavingProfileSlide.tsx
@@ -1,40 +1,115 @@
 import React from "react";
 import { Selector, Slide } from "@martynasj/quiz-lib";
 
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Slide as SlideAnimation } from "@chakra-ui/react";
 import { TestimonialCard } from "@components/TestimonialCard";
 import { StaticImage } from "gatsby-plugin-image";
-import { SlideHeading, SpanJust } from "../components";
+import { NextButton, SlideHeading } from "../components";
+
+const testimonials = [
+  {
+    name: "Sarah",
+    quote: `I've always loved astrology, but never had insights made just for me. It's been surprisingly helpful in understanding my partner and dealing with tough times more kindly. Now, we're learning to appreciate our differences`,
+    question: "",
+    avatar: <StaticImage alt="Astropal user image" src="../../../images/user3.png" />,
+    commentAge: "7d",
+  },
+  {
+    name: "Michelle Richards",
+    quote:
+      "Our marriage got a major upgrade thanks to the insights – turning challenges into growth moments!",
+    question: "",
+    avatar: <StaticImage alt="Astropal user image" src="../../../images/user2.png" />,
+    commentAge: "4d",
+  },
+  {
+    name: "Thomas",
+    quote: "The insights and ideas are valuable. Great product!",
+    question: "",
+    avatar: <StaticImage alt="Astropal user image" src="../../../images/user6.png" />,
+    commentAge: "2d",
+  },
+];
 
 export function SavingProfileSlide() {
+  const [testimonialIdx, setTestimonialIdx] = React.useState<number>(0);
+  const [showInput, setShowInput] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const timeout = setInterval(() => {
+      setTestimonialIdx((idx) => (idx + 1 <= testimonials.length - 1 ? idx + 1 : 0));
+    }, 3000);
+
+    return () => clearInterval(timeout);
+  }, []);
+
   return (
-    <Slide id="saving-profile" type="loading" duration={3} autoProceed={true}>
-      <SlideHeading text={<SpanJust>Saving your profile</SpanJust>} />
+    <Slide
+      id="saving-profile"
+      type="loading"
+      duration={4}
+      onLoadingCompleted={() => setShowInput(true)}
+    >
+      <SlideHeading
+        textAlign={"center"}
+        text="Improving your Astrological Profile to offer the best insights"
+      />
 
       <Flex flexDirection={"column"} alignItems={"center"}>
         <Box mt={5}>
           <Selector />
         </Box>
 
-        <Text textAlign={"center"} color="brand.600" fontSize={"3xl"} fontWeight={"black"}>
-          31,000+ people
+        {showInput && (
+          <NextButton mb={5} mt={3}>
+            Continue
+          </NextButton>
+        )}
+
+        <Text textAlign={"center"} color="brand.700" fontWeight={"semibold"} fontSize={"lg"} mb={5}>
+          See what others say about Astropal:
         </Text>
 
-        <Text textAlign={"center"} color="white" fontWeight={"bold"} fontSize={"lg"} mb={5}>
-          have chosen Astropal
-        </Text>
-
-        <TestimonialCard
-          testimonial={{
-            name: "Michelle Richards",
-            quote:
-              "Our marriage got a major upgrade thanks to the guide – turning challenges into growth moments!",
-            question: "Is it a good time to invest my savings?",
-            avatar: <StaticImage alt="Astropal user image" src="../../../images/user2.png" />,
-            commentAge: "4d",
-          }}
-        />
+        <Box
+          color="black"
+          boxShadow={"lg"}
+          p={4}
+          bg="white"
+          position={"relative"}
+          width={"full"}
+          height={240}
+          overflow={"hidden"}
+        >
+          {testimonials.map((it, idx) => {
+            return (
+              <SlideAnimation
+                key={it.name}
+                in={testimonialIdx === idx}
+                unmountOnExit
+                transition={{ exit: { duration: 0 }, enter: { duration: 0.7 } }}
+                style={{
+                  position: "absolute",
+                  width: "90%",
+                  left: "5%",
+                  top: "15px",
+                }}
+              >
+                <TestimonialCard testimonial={it} />
+              </SlideAnimation>
+            );
+          })}
+        </Box>
       </Flex>
     </Slide>
   );
+}
+
+{
+  /* <Text textAlign={"center"} color="brand.600" fontSize={"3xl"} fontWeight={"black"}>
+31,000+ people
+</Text>
+
+<Text textAlign={"center"} color="white" fontWeight={"bold"} fontSize={"lg"} mb={5}>
+have chosen Astropal
+</Text> */
 }
