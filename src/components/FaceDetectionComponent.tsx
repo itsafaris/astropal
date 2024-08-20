2e035e54a6afde4af0fd5ca4d059109806dfddb0
martynasj <martyjankauskas@gmail.com>
2024-08-20 17:37:10 +0300

    Fix styling

--- src/components/FaceDetectionComponent.tsx
+++ src/components/FaceDetectionComponent.tsx
@@ -41,6 +41,8 @@ export function FaceDetectionComponent({
   imgDataUrl,
   onAnalysisComplete,
 }: IFaceDetectionComponentProps) {
+  const scanningAnimationDuration = 5;
+
   const $canvas = React.useRef<HTMLCanvasElement>(null);
   const { faceLandmarker } = useServices();
 
@@ -87,8 +89,37 @@ export function FaceDetectionComponent({
     const drawingUtils = new DrawingUtils(ctx);
 
     for (const landmarks of result.faceLandmarks) {
+      const c1 = "#07bfc4b3";
       drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_TESSELATION, {
-        color: "#42c99a",
+        color: "#07bfc421",
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS, {
+        color: c1,
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS, {
+        color: c1,
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE, {
+        color: c1,
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_EYE, {
+        color: c1,
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW, {
+        color: c1,
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW, {
+        color: c1,
+        lineWidth: 1,
+      });
+      drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LIPS, {
+        color: c1,
         lineWidth: 1,
       });
     }
@@ -106,55 +137,48 @@ export function FaceDetectionComponent({
   }
 
   return (
-    <Box p={8}>
-      <Box
-        position={"relative"}
-        display={"inline-block"}
-        transform={faceReadingState.type === "interpreting" ? "scale(0.6)" : "scale(1)"}
-        transformOrigin={"top center"}
-        transition={"transform 2s"}
-        onTransitionEnd={() => {
-          setFaceReadingState((s) => ({ ...s, phase: 1 }));
-        }}
-      >
-        {imgDataUrl && (
-          <Box position={"relative"}>
-            <Image src={imgDataUrl} width={600} />
-            {faceReadingState.type === "scanning" && (
-              <Box
-                width={"100%"}
-                animation={"handPoints 3s ease-in-out"}
-                bg="teal.300"
-                opacity={0.5}
-                position={"absolute"}
-                top={0}
-                left={0}
-                borderBottom={"4px solid green"}
-                onAnimationEnd={() => {
-                  setFaceReadingState({ type: "interpreting", phase: 0 });
-                }}
-              />
-            )}
-          </Box>
-        )}
-        <canvas
-          ref={$canvas}
-          width={"800px"}
-          height={"999px"}
-          style={{
-            left: 0,
-            top: 0,
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            pointerEvents: "none",
-            visibility: faceReadingState.type === "scanning" ? "hidden" : "visible",
-          }}
-        />
-      </Box>
+    <Box>
+      <Flex position={"relative"} justifyContent={"center"}>
+        <Box position={"relative"}>
+          {imgDataUrl && (
+            <Box position={"relative"}>
+              <Image src={imgDataUrl} maxHeight={320} mx="auto" borderRadius={"xl"} />
+              {faceReadingState.type === "scanning" && (
+                <Box
+                  width={"100%"}
+                  animation={`handPoints ${scanningAnimationDuration}s ease-in-out`}
+                  bg="teal.300"
+                  opacity={0.5}
+                  position={"absolute"}
+                  top={0}
+                  left={0}
+                  borderBottom={"4px solid green"}
+                  onAnimationEnd={() => {
+                    setFaceReadingState({ type: "interpreting", phase: 1 });
+                  }}
+                />
+              )}
+            </Box>
+          )}
+          <canvas
+            ref={$canvas}
+            width={"800px"}
+            height={"999px"}
+            style={{
+              left: 0,
+              top: 0,
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              pointerEvents: "none",
+              visibility: faceReadingState.type === "scanning" ? "hidden" : "visible",
+            }}
+          />
+        </Box>
+      </Flex>
       <Box>
         {faceReadingState.type === "interpreting" && (
-          <Box position={"relative"} top={-20}>
+          <Box position={"relative"} mx={8}>
             {faceReadingState.phase === 0 ? null : (
               <LoadingComponent onComplete={onAnalysisComplete} />
             )}
@@ -166,6 +190,8 @@ export function FaceDetectionComponent({
 }
 
 function LoadingComponent({ onComplete }: { onComplete?: () => void }) {
+  const totalLoadingTime = 8;
+
   const [phase, setPhase] = React.useState(0);
 
   React.useEffect(() => {
@@ -173,6 +199,7 @@ function LoadingComponent({ onComplete }: { onComplete?: () => void }) {
   }, []);
 
   async function startAnalysisPhases() {
+    const intervalTime = (totalLoadingTime * 1000) / 100;
     const i = setInterval(() => {
       setPhase((v) => {
         const next = v + 1;
@@ -182,13 +209,13 @@ function LoadingComponent({ onComplete }: { onComplete?: () => void }) {
         }
         return next;
       });
-    }, 50);
+    }, intervalTime);
   }
 
   return (
-    <Box>
-      <Progress value={phase} borderRadius={"full"} colorScheme="brand" mb={2} />
-      <Flex direction={"column"} gap={3} fontWeight={"bold"}>
+    <Box mt={2}>
+      <Progress size={"sm"} value={phase} borderRadius={"full"} colorScheme={"green"} mb={2} />
+      <Flex direction={"column"} gap={1} fontWeight={"bold"}>
         <LoadingListItem isComplete={phase >= 25} text="Reading the face data"></LoadingListItem>
         <LoadingListItem isComplete={phase >= 50} text="Analysing results" />
         <LoadingListItem isComplete={phase >= 75} text="Forecasting destiny" />
@@ -202,7 +229,9 @@ function LoadingListItem({ isComplete, text }: { isComplete?: boolean; text: str
   return (
     <Flex gap={2} alignItems={"center"}>
       <Icon as={FaCheck} color={isComplete ? "green.500" : "text.200"} />
-      <Text color={isComplete ? "text" : "text.200"}>{text}</Text>
+      <Text color={isComplete ? "text" : "text.200"} fontSize={"sm"}>
+        {text}
+      </Text>
     </Flex>
   );
 }
--- src/components/faceReading/faceScanAnalysisSlide.tsx
+++ src/components/faceReading/faceScanAnalysisSlide.tsx
@@ -1,21 +1,27 @@
+import { Box } from "@chakra-ui/react";
 import { FaceDetectionComponent } from "@components/FaceDetectionComponent";
+import { NextButton } from "@components/quizpage/components";
 import { useGlobalState2 } from "@components/root/RootWrapper";
-import { Slide, useQuiz } from "@martynasj/quiz-lib/index";
+import { Slide } from "@martynasj/quiz-lib/index";
+import { useState } from "react";
 
 export interface IFaceScanAnalysisSlideProps {}
 
 export function FaceScanAnalysisSlide(props: IFaceScanAnalysisSlideProps) {
+  const [isComplete, setIsComplete] = useState(false);
   const globalState = useGlobalState2();
-  const { submitQuestion } = useQuiz();
 
   return (
     <Slide id="face-scan-analysis" type="filler">
-      <FaceDetectionComponent
-        imgDataUrl={globalState.faceImageDataUrl}
-        onAnalysisComplete={() => {
-          submitQuestion();
-        }}
-      />
+      <Box mb={4} mt={-3}>
+        <FaceDetectionComponent
+          imgDataUrl={globalState.faceImageDataUrl}
+          onAnalysisComplete={() => {
+            setIsComplete(true);
+          }}
+        />
+      </Box>
+      {isComplete && <NextButton>Continue</NextButton>}
     </Slide>
   );
 }
