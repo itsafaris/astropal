a64eca6ba0f9f79a8d4410a3bbe75cd612f0d2f2
Vilius Linge <viliusling@gmail.com>
2024-10-07 15:46:52 +0300

    Add page wrapper to ssr config

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import { GatsbySSR } from "gatsby";
-import { RootWrapper } from "./src/components/root/RootWrapper";
+import { RootWrapper } from "./src/components/wrappers/RootWrapper";
+import { PageWrapper } from "./src/components/wrappers/PageWrapper";
 
 export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({ element }) => {
   return <RootWrapper>{element}</RootWrapper>;
+};
+
+export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element }) => {
+  return <PageWrapper>{element}</PageWrapper>;
 };
