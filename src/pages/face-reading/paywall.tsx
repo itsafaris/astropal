511f7f47076f91e75f104f9ffcc121ad12edb27e
Vilius Linge <viliusling@gmail.com>
2024-10-22 16:29:00 +0300

    Add topnav to paywal step

--- src/pages/face-reading/paywall.tsx
+++ src/pages/face-reading/paywall.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { TopNavigation } from "@components/topnavigation";
 import { useGlobalState2, useGlobalUpdate2 } from "@components/wrappers/RootWrapper";
 import { useSiteMetadata } from "@hooks/useSiteMetadata";
 import { navigate } from "gatsby";
@@ -17,58 +18,62 @@ export default function PaywallPage(props: IPaywallPageProps) {
   const compensationPrice = donationPrices[donationPrices.length - 1];
 
   return (
-    <Container as={Flex} direction={"column"} alignItems={"center"} justifyContent={"center"}>
-      <Heading mt={8} mb={8} fontSize={"xl"} textAlign={"center"}>
-        Try {meta.brandName} for 1 week
-      </Heading>
-      <Text fontSize={"lg"} textAlign={"center"}>
-        Join millions who have unlocked the secrets of their future in love, life, and family with
-        our personalized face readings.
-      </Text>
-      <Box my={4}>
-        <StaticImage
-          alt="shooting star icon"
-          src="../../images/shooting-star.png"
-          placeholder="none"
-          style={{ width: 160, margin: "0 auto" }}
-          width={160}
-        />
-      </Box>
+    <Box>
+      <TopNavigation />
 
-      <Flex gap={2} my={8}>
-        {donationPrices.map((price) => {
-          return (
-            <Button
-              key={price.priceID}
-              size="lg"
-              px={2}
-              variant={"outline"}
-              borderWidth={2}
-              borderColor={
-                globalState.selectedPricingPlan === price.priceID ? "brand.600" : "gray.300"
-              }
-              onClick={() => {
-                updateState((s) => ({ ...s, selectedPricingPlan: price.priceID }));
-              }}
-            >
-              ${(price.unit_amount / 100).toFixed(2)}
-            </Button>
-          );
-        })}
-      </Flex>
-      <Text fontSize={"sm"} fontWeight={"bold"} textAlign={"center"}>
-        It costs us ${compensationPrice?.unit_amount / 100} to compensate our {meta.brandName}{" "}
-        employees for the trial, but please choose the amount you are comfortable with.
-      </Text>
+      <Container as={Flex} direction={"column"} alignItems={"center"} justifyContent={"center"}>
+        <Heading mt={8} mb={8} fontSize={"xl"} textAlign={"center"}>
+          Try {meta.brandName} for 1 week
+        </Heading>
+        <Text fontSize={"lg"} textAlign={"center"}>
+          Join millions who have unlocked the secrets of their future in love, life, and family with
+          our personalized face readings.
+        </Text>
+        <Box my={4}>
+          <StaticImage
+            alt="shooting star icon"
+            src="../../images/shooting-star.png"
+            placeholder="none"
+            style={{ width: 160, margin: "0 auto" }}
+            width={160}
+          />
+        </Box>
 
-      <Button
-        width={"full"}
-        colorScheme="brand"
-        mt={4}
-        onClick={() => navigate("/face-reading/summary")}
-      >
-        Continue
-      </Button>
-    </Container>
+        <Flex gap={2} my={8}>
+          {donationPrices.map((price) => {
+            return (
+              <Button
+                key={price.priceID}
+                size="lg"
+                px={2}
+                variant={"outline"}
+                borderWidth={2}
+                borderColor={
+                  globalState.selectedPricingPlan === price.priceID ? "brand.600" : "gray.300"
+                }
+                onClick={() => {
+                  updateState((s) => ({ ...s, selectedPricingPlan: price.priceID }));
+                }}
+              >
+                ${(price.unit_amount / 100).toFixed(2)}
+              </Button>
+            );
+          })}
+        </Flex>
+        <Text fontSize={"sm"} fontWeight={"bold"} textAlign={"center"}>
+          It costs us ${compensationPrice?.unit_amount / 100} to compensate our {meta.brandName}{" "}
+          employees for the trial, but please choose the amount you are comfortable with.
+        </Text>
+
+        <Button
+          width={"full"}
+          colorScheme="brand"
+          mt={4}
+          onClick={() => navigate("/face-reading/summary")}
+        >
+          Continue
+        </Button>
+      </Container>
+    </Box>
   );
 }
