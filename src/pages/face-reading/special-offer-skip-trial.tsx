7090e68119bd12944a987ea5158e8e7768588a97
Vilius Linge <viliusling@gmail.com>
2024-11-12 15:01:46 +0200

    finish first special offer page

--- src/pages/face-reading/special-offer-skip-trial.tsx
+++ src/pages/face-reading/special-offer-skip-trial.tsx
@@ -2,7 +2,9 @@ import {
   Box,
   Button,
   Container,
+  Flex,
   Grid,
+  Icon,
   Progress,
   Stack,
   Step,
@@ -14,8 +16,24 @@ import {
   useSteps,
 } from "@chakra-ui/react";
 import { TopNavigation } from "@components/topnavigation";
+import React from "react";
+
+import { PiHandPalmLight } from "react-icons/pi";
+import { LuCalendarCheck2 } from "react-icons/lu";
+import { GiSelfLove } from "react-icons/gi";
+import { MdOutlineTipsAndUpdates } from "react-icons/md";
 
 export default function SpecialOfferSkipTrial() {
+  const [hasSkipped, setHasSkipped] = React.useState(false);
+
+  function handlePurchase() {}
+
+  function handleFirstSkip() {
+    setHasSkipped(true);
+  }
+
+  function handleSecondSkip() {}
+
   return (
     <Box>
       <TopNavigation />
@@ -24,21 +42,124 @@ export default function SpecialOfferSkipTrial() {
         <Stack textAlign={"center"} spacing={6}>
           <SpecialOfferSteps activeStepIdx={1} />
 
-          <Text fontSize={"xl"} fontWeight={"bold"}>
-            Not planning on looking back?
-          </Text>
+          {hasSkipped ? (
+            <Stack spacing={6}>
+              <Text fontSize={"xl"} fontWeight={"bold"}>
+                Are you sure?
+              </Text>
 
-          <Grid gridTemplateColumns={"1fr 1fr"} gap={2} alignItems={"flex-end"}>
-            <Card />
-            <CardSpecial />
-          </Grid>
+              <Text lineHeight={1.3} fontSize={"sm"}>
+                Let our expert astrologer and face reader guide you 24/7 on your journey to life's
+                answers.
+              </Text>
+
+              <Stack spacing={4}>
+                <Text fontSize={"md"} fontWeight={"bold"} color={"brand.500"}>
+                  What you get:
+                </Text>
+
+                <Stack mx="auto">
+                  <Flex alignItems={"center"} gap={2}>
+                    <Icon
+                      as={PiHandPalmLight}
+                      color={"brand.500"}
+                      backgroundColor={"brand.100"}
+                      boxSize={9}
+                      p={2}
+                      borderRadius={"md"}
+                    />
+                    <Text fontWeight={"semibold"} color={"brand.500"}>
+                      Unlimited palm readings
+                    </Text>
+                  </Flex>
+
+                  <Flex alignItems={"center"} gap={2}>
+                    <Icon
+                      as={GiSelfLove}
+                      color={"brand.500"}
+                      backgroundColor={"brand.100"}
+                      boxSize={9}
+                      p={2}
+                      borderRadius={"md"}
+                    />
+                    <Text fontWeight={"semibold"} color={"brand.500"}>
+                      Daily compatibility readings
+                    </Text>
+                  </Flex>
+
+                  <Flex alignItems={"center"} gap={2}>
+                    <Icon
+                      as={MdOutlineTipsAndUpdates}
+                      color={"brand.500"}
+                      backgroundColor={"brand.100"}
+                      boxSize={9}
+                      p={2}
+                      borderRadius={"md"}
+                    />
+                    <Text fontWeight={"semibold"} color={"brand.500"}>
+                      Cosmic relationships tips
+                    </Text>
+                  </Flex>
+
+                  <Flex alignItems={"center"} gap={2}>
+                    <Icon
+                      as={LuCalendarCheck2}
+                      color={"brand.500"}
+                      backgroundColor={"brand.100"}
+                      boxSize={9}
+                      p={2}
+                      borderRadius={"md"}
+                    />
+                    <Text fontWeight={"semibold"} color={"brand.500"}>
+                      Daily horoscopes
+                    </Text>
+                  </Flex>
+                </Stack>
+              </Stack>
+
+              <Stack spacing={3}>
+                <Box height={"2px"} width={"full"} backgroundColor={"gray.200"} />
+
+                <Text fontSize={"sm"} color={"orange.500"}>
+                  Are you sure you don't want to save $84 per year?
+                </Text>
+
+                <Flex mx={"auto"} gap={2} alignItems={"center"} width={"full"}>
+                  <Button size={"lg"} py={7} onClick={handleSecondSkip}>
+                    <Text fontSize={["sm", "md"]}>Skip</Text>
+                  </Button>
+
+                  <Button
+                    size={"lg"}
+                    py={7}
+                    colorScheme="brand"
+                    flexGrow={1}
+                    onClick={handlePurchase}
+                  >
+                    <Text fontSize={["sm", "md"]}>Accept this offer</Text>
+                  </Button>
+                </Flex>
+              </Stack>
+            </Stack>
+          ) : (
+            <Stack>
+              <Text fontSize={"xl"} fontWeight={"bold"}>
+                Not planning on looking back?
+              </Text>
+
+              <Grid gridTemplateColumns={"1fr 1fr"} gap={2} alignItems={"flex-end"}>
+                <Card onSelect={handleFirstSkip} />
+                <CardSpecial onSelect={handlePurchase} />
+              </Grid>
+            </Stack>
+          )}
         </Stack>
       </Container>
     </Box>
   );
 }
 
-function Card() {
+function Card({ onSelect }: { onSelect: () => void }) {
   return (
     <Stack
       p={3}
@@ -68,24 +189,24 @@ function Card() {
         <Text color="gray.500">$76</Text>
       </Stack>
 
-      <Button size={"lg"} py={7}>
+      <Button size={"lg"} py={7} onClick={onSelect}>
         <Text fontSize={["sm", "md"]}>Start trial</Text>
       </Button>
     </Stack>
   );
 }
 
-function CardSpecial() {
+function CardSpecial({ onSelect }: { onSelect: () => void }) {
   return (
     <Stack
       border={"2px solid"}
-      borderColor={"blue.500"}
+      borderColor={"brand.500"}
       borderRadius={"xl"}
       spacing={0}
       fontSize={["sm", "md"]}
       overflow={"hidden"}
     >
-      <Box backgroundColor={"blue.500"} py={3}>
+      <Box backgroundColor={"brand.500"} py={3}>
         <Text textAlign={"center"} color={"white"} fontWeight={"semibold"}>
           BEST VALUE 🎉
         </Text>
@@ -111,7 +232,7 @@ function CardSpecial() {
           <Text color="gray.500">$69</Text>
         </Stack>
 
-        <Button size={"lg"} py={7} colorScheme="blue">
+        <Button size={"lg"} py={7} colorScheme="brand" onClick={onSelect}>
           <Text fontSize={["sm", "md"]}>
             Skip trial <br />
             and accept offer
@@ -147,7 +268,7 @@ function SpecialOfferSteps({ activeStepIdx = 1 }: { activeStepIdx?: number }) {
       </Grid>
 
       <Box position="relative">
-        <Stepper size="sm" index={activeStep} gap="0">
+        <Stepper size="sm" index={activeStep} gap="0" colorScheme="brand">
           {steps.map((_, index) => (
             <Step key={index} gap="0">
               <StepIndicator bg="white">
@@ -164,6 +285,7 @@ function SpecialOfferSteps({ activeStepIdx = 1 }: { activeStepIdx?: number }) {
           width="full"
           top="10px"
           zIndex={-1}
+          colorScheme="brand"
         />
       </Box>
     </Stack>
